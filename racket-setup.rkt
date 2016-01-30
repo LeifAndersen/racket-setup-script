f4876d2abdb5b5fb1c03e62b1e37d799a2bf40d3
Leif Andersen <leif@leifandersen.net>
2016-01-30 12:32:55 -0500

    Initial commit.

---
+++ racket-setup.rkt
@@ -0,0 +1,107 @@
+#lang racket
+
+(require compiler/find-exe
+         pkg/lib)
+
+(define pkgs '("scribble"
+               "typed-racket"
+               "gui"
+               "redex"
+               "readline"
+               "compiler"
+               "slideshow"
+               "xrepl"
+               "plot"
+               "pict"
+               "profile"
+               "optimization-coach"
+               "games"
+               "unstable"
+               "benchmark"
+               "plt-web"
+               "pkg-index"
+               "distro-build"
+               "reloadable"
+               "aws"
+               "racket-lang-org"
+               "cKanren"
+               "minikanren"
+               "glob"
+               "parsack"
+               "markdown"
+               "zordoz"
+               "slideshow-pretty"
+               "slideshow-latex"
+               "slideshow-repl"
+               "prospect"
+               "prospect-gl"
+               "raco-find-collection"
+               "threading"
+               "rackjure"
+               "frog"
+               "cow-repl"
+               "tzinfo"
+               "tzdata"
+               "gregor"
+               "ppict"
+               "python"
+               ;"c"
+               "c-utils"
+               "c-defs"
+               "css-tools"
+               "cover"
+               "cover-coveralls"
+               "doc-coverage"
+               "dlm-read"
+               "drracket-vim-tool"
+               "feature-profile"
+               "fulmar"
+               "irc"
+               "irc-client"
+               "racket-cheat"
+               "puresuri"
+               "python-tokenizer"
+               "quickcheck"
+               "superc"
+               "sweet-exp"
+               "rsound"
+               "syntax-lang"
+               "remote-shell"
+               "script-plugin"
+               "txexpr"
+               "pollen"
+               "debug"
+               "colon-kw"
+               "colon-match"
+               "sicp"
+               "pfds"
+               "while-loop"
+               "control"
+               "hygenic-quote-lang"
+               "collections"
+               "trivial"
+               "pdf-read"
+               "racket-poppler"
+               "datatype"
+               "portaudio"
+               "point-free"
+               "adapton"
+               ;"github"
+               "github-api"))
+(define planet-pkgs '("jowalsh/code-coverage"
+                      ;"dyoo/bf"
+                      "neil/scribble-emacs"
+                      "neil/csv:1:=7"))
+
+(parameterize ([current-directory "/Users/leif/rsrc"])
+  (for ([i (in-list pkgs)])
+    (cond [(hash-has-key? (installed-pkg-table) i)
+           (match (hash-ref (installed-pkg-table) i)
+             [(struct* pkg-info ([orig-pkg `(clone ,_ ,_)]))
+              (system* (find-exe) "-l" "raco" "pkg" "update" "--deps" "search-auto" i)]
+             [else
+              (system* (find-exe) "-l" "raco" "pkg" "update" "--deps" "search-auto" "--clone" i)])]
+          [else
+           (system* (find-exe) "-l" "raco" "pkg" "install" "--deps" "search-auto" "--clone" i)]))
+  (for ([i (in-list planet-pkgs)])
+    (system* (find-exe) "-e" (format "(require (planet ~a))" i))))
