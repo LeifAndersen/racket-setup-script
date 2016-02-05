577cf7fa33329692bbed067cc712c205485169ee
Leif Andersen <leif@leifandersen.net>
2016-02-05 17:54:16 -0500

    Get repo for DrRacket.

--- racket-setup.rkt
+++ racket-setup.rkt
@@ -6,6 +6,7 @@
 (define pkgs '("scribble"
                "typed-racket"
                "gui"
+               "drracket"
                "redex"
                "readline"
                "compiler"
